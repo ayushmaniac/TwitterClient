e35759c155f90e4b81efd0c068b10fd762a32167
ayushshrivastava <ayush.shrivastava@rebelfoods.com>
2020-11-02 14:07:17 +0530

    Splash Activity Done. Little Less Left :)
    Thanks for entertaining my profile.
    I'm not so good with Lottie :D

--- app/src/main/java/com/client/twitter/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/client/twitter/ui/splash/SplashActivity.kt
@@ -1,17 +1,36 @@
 package com.client.twitter.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import com.client.twitter.R
 import com.client.twitter.di.component.ActivityComponent
 import com.client.twitter.ui.base.BaseActivity
+import com.client.twitter.ui.main.ui.MainActivity
 
 class SplashActivity : BaseActivity<SplashViewModel>() {
 
-    override fun injectDependencies(activityComponent: ActivityComponent) = activityComponent.inject(this)
+    private val SPLASH_SCREEN_TIME_OUT : Long= 3000
+
+    override fun injectDependencies(activityComponent: ActivityComponent) = activityComponent.inject(
+        this
+    )
 
     override fun provideLayoutId(): Int = R.layout.activity_splash
 
     override fun setupView(savedInstanceState: Bundle?) {
+        startSplash()
+    }
+
+    private fun startSplash() {
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_SCREEN_TIME_OUT)
     }
 }
